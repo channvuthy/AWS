f03a4ac97f11c8904cf9457ee5381f4bc008cfbc
chann vuthy <vuthychan271@yahoo.com>
2017-03-27 09:34:58 +0700

    add other file

---
+++ phpaws/s3/app/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    's3' => [
+        'key' => 'AKIAJIRZMMG6N27JPYNA',
+        'secret' => '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',
+        'bucket' => 'plan-b-amazon'
+    ]
+];
+?>
---
+++ phpaws/s3/download.php
@@ -0,0 +1,17 @@
+<?php
+require('app/start.php');
+$object = "uploads/Chrysanthemum.jpg";
+$url = $s3->getObjectUrl($config['s3']['bucket'], $object, '5 minutes');
+$download = $s3->downloadBucket("files/", $config['s3']['bucket']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Token</title>
+</head>
+<body>
+<a href="<?php echo $url; ?>">Download</a>
+</body>
+</html>
